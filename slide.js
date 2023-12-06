4b645f8a8fca1d81533de3794a548b19f7692294
kimnabim <ngim1731@naver.com>
2023-12-06 14:21:40 +0900

    #

---
+++ slide.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    $('#slide1>ul').delay(2500);
+    $('#slide1>ul').animate({marginLeft: "-500px"})
+    $('#slide1>ul').delay(2500);
+    $('#slide1>ul').animate({marginLeft: "-1000px"})
+    $('#slide1>ul').delay(2500);
+    $('#slide1>ul').animate({marginLeft: "0px"})
+});
